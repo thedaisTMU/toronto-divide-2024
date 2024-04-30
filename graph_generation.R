529d09aa8f8baceb59b4a3c76b0ab91b1c58d30c
angusalockhart <116596499+angusalockhart@users.noreply.github.com>
2024-04-30 14:45:49 -0400

    Add files via upload

---
+++ graph_generation.R
@@ -0,0 +1,202 @@
+library(DaisTheme)
+library(ggplot2)
+library(data.table)
+
+
+
+#########################################################
+#Load in graph spread sheets
+graph.data <- fread("C:/Users/alockhart/Desktop/Toronto Survey/Graphs/Graphs_spreadsheet.csv")
+
+
+
+
+#################################
+#Figure 1 onwards
+
+
+figure_1_data <- fread("C:/Users/alockhart/Desktop/Toronto Survey/Graphs/Figure_1.csv")
+figure.1 <- plot.column.dais(figure_1_data, stat, year, order.bar="ascending", group.by=description, label=FALSE,
+                           plot.fig.num = "Figure 1",
+                           plot.title= graph.data[graph.data$Figure_number=="Figure 1",Figure_title],
+                           y.axis= graph.data[graph.data$Figure_number=="Figure 1",Y_Axis],
+                           caption = graph.data[graph.data$Figure_number=="Figure 1",Caption],
+                           label.unit = graph.data[graph.data$Figure_number=="Figure 1",Y_Axis_Ticks],
+                           label.adjust = 0,
+                           export = TRUE,
+                           export.name = "F1") + 
+  scale_x_discrete(guide = guide_axis(angle = 0)) + 
+  geom_text(aes(label = paste0(stat, "%")), position = position_dodge(0.6), vjust=-0.15)
+
+#FIX
+figure_3_data <- fread("C:/Users/alockhart/Desktop/Toronto Survey/Graphs/Figure_3.csv")
+figure_3_data$description <- factor(figure_3_data$description, levels=unique(figure_3_data$description))
+figure_3_data$Year <- as.character(figure_3_data$Year)
+#figure.3 <- plot.column.dais(figure_3_data, stat, year, order.bar="ascending", group.by=description, label=TRUE,
+#                             plot.fig.num = "Figure 3",
+#                             plot.title= graph.data[graph.data$Figure_number=="Figure 3",Figure_title],
+#                             y.axis= graph.data[graph.data$Figure_number=="Figure 3",Y_Axis],
+#                             caption = graph.data[graph.data$Figure_number=="Figure 3",Caption],
+#                             label.unit = graph.data[graph.data$Figure_number=="Figure 3",Y_Axis_Ticks],
+#                             label.adjust = 0.025,
+#                             export = TRUE,
+#                             export.name = "F3",
+#                             colours=c("#eb0071", "#ec3479", "#ed4c82", "#ed608b",
+#                                       "#ed7194", "#ed819d", "#ec8fa6", "#ea9eaf",
+#                                       "#e8acb9" ,"#e5bac2" ,"#e1c7cc"))
+
+
+figure.3 <- ggplot2::ggplot(figure_3_data, aes(fill=Year, x = description, y = stat)) +
+  geom_bar(position="dodge2", stat="identity") + 
+  labs(title = "Access Locations", y = "% of those without home internet") + 
+  scale_fill_manual(values = c("2023"="#eb0072", "2020"="#000000"))+
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+  geom_text(aes(label = paste0(stat, "%")), position = position_dodge(0.85), vjust=-0.15, size=3)+
+  labs(title = "Figure 3",subtitle = graph.data[Figure_number=="Figure 3",Figure_title], caption = graph.data[Figure_number=="Figure 3",Caption],
+         fill = "Legend")+
+  guides(colour="none")+
+  dais.base.theme() +
+  theme(axis.title.x=element_blank())
+#  geom_text(
+#    nudge_x=0.45, nudge_y=0.1,
+#    check_overlap=T,
+#    size=3,
+#    colour="black"
+#  )+
+#  labs(title = "Figure 6",subtitle = graph.data[Figure_number=="Figure 6",Figure_title], caption = graph.data[Figure_number=="Figure 6",Caption],
+#       fill = "Legend")+ 
+#  guides(colour="none")
+
+figure_4_data <- fread("C:/Users/alockhart/Desktop/Toronto Survey/Graphs/Figure_4.csv")
+figure.4 <- plot.column.dais(figure_4_data, stat, description, order.bar="descending", label=FALSE,
+                             plot.fig.num = "Figure 4",
+                             plot.title= graph.data[graph.data$Figure_number=="Figure 4",Figure_title],
+                             y.axis= graph.data[graph.data$Figure_number=="Figure 4",Y_Axis],
+                             caption = graph.data[graph.data$Figure_number=="Figure 4",Caption],
+                             label.unit = graph.data[graph.data$Figure_number=="Figure 4",Y_Axis_Ticks],
+                             label.adjust = 0.025,
+                             export = TRUE,
+                             export.name = "F4") + 
+  geom_text(aes(label = paste0(stat, "%")), position = position_dodge(0.6), vjust=-0.15)
+
+
+figure_5_data <- fread("C:/Users/alockhart/Desktop/Toronto Survey/Graphs/Figure_5.csv")
+figure.5 <- plot.column.dais(figure_5_data, stat, description, order.bar="descending", label=TRUE,
+                             plot.fig.num = "Figure 5",
+                             plot.title= graph.data[graph.data$Figure_number=="Figure 5",Figure_title],
+                             y.axis= graph.data[graph.data$Figure_number=="Figure 5",Y_Axis],
+                             caption = graph.data[graph.data$Figure_number=="Figure 5",Caption],
+                             label.unit = graph.data[graph.data$Figure_number=="Figure 5",Y_Axis_Ticks],
+                             label.adjust = 0.025,
+                             export = TRUE,
+                             export.name = "F5") + 
+  geom_text(aes(label = paste0(stat, "%")), position = position_dodge(0.6), vjust=-0.15)
+
+figure_6_data <- fread("C:/Users/alockhart/Desktop/Toronto Survey/Graphs/Figure_6.csv")
+figure_6_data$description <- factor(figure_6_data$description, levels=unique(figure_6_data$description))
+figure_6_data$year <- as.character(figure_6_data$year)
+figure.6 <- plot.column.dais(figure_6_data, stat, year, order.bar="descending", group.by=description, label=FALSE,
+                             plot.fig.num = "Figure 6",
+                             plot.title= graph.data[graph.data$Figure_number=="Figure 6",Figure_title],
+                             y.axis= graph.data[graph.data$Figure_number=="Figure 6",Y_Axis],
+                             caption = graph.data[graph.data$Figure_number=="Figure 6",Caption],
+                             label.unit = graph.data[graph.data$Figure_number=="Figure 6",Y_Axis_Ticks],
+                             label.adjust = 0.025,
+                             export = TRUE,
+                             export.name = "F6",
+                             colours=c("#eb0072","#7474c1","#004c9b","#00a9ef","#6bbfae","#ffdc00","#ff7200")) + 
+  scale_x_discrete(guide = guide_axis(angle = 0)) + 
+  geom_text(aes(label = paste0(stat, "%")), position = position_dodge(0.6), vjust=-0.15)
+
+figure_7_data <- fread("C:/Users/alockhart/Desktop/Toronto Survey/Graphs/Figure_7.csv")
+figure_7_data$description <- factor(figure_7_data$description, levels=unique(figure_7_data$description))
+figure_7_data$year <- as.character(figure_7_data$year)
+figure.7 <- plot.column.dais(figure_7_data, stat, year, group.by=description, label=FALSE,
+                             plot.fig.num = "Figure 7",
+                             plot.title= graph.data[graph.data$Figure_number=="Figure 7",Figure_title],
+                             y.axis= graph.data[graph.data$Figure_number=="Figure 7",Y_Axis],
+                             caption = graph.data[graph.data$Figure_number=="Figure 7",Caption],
+                             label.unit = graph.data[graph.data$Figure_number=="Figure 7",Y_Axis_Ticks],
+                             label.adjust = 0.025,
+                             export = TRUE,
+                             export.name = "F7",
+                             colours=set.colours(5,type = "gradient", gradient.choice = "hot.pink")) + 
+  scale_x_discrete(guide = guide_axis(angle = 0)) + 
+  geom_text(aes(label = paste0(stat, "%")), position = position_dodge(0.6), vjust=-0.15)
+
+
+figure_8_data <- fread("C:/Users/alockhart/Desktop/Toronto Survey/Graphs/Figure_8.csv")
+figure_8_data$description <- factor(figure_8_data$description, levels=unique(figure_8_data$description))
+figure_8_data$year <- as.character(figure_8_data$year)
+figure.8 <- plot.column.dais(figure_8_data, stat, year, group.by=description, label=TRUE,
+                             plot.fig.num = "Figure 8",
+                             plot.title= graph.data[graph.data$Figure_number=="Figure 8",Figure_title],
+                             y.axis= graph.data[graph.data$Figure_number=="Figure 8",Y_Axis],
+                             caption = graph.data[graph.data$Figure_number=="Figure 8",Caption],
+                             label.unit = graph.data[graph.data$Figure_number=="Figure 8",Y_Axis_Ticks],
+                             label.adjust = 0.025,
+                             export = TRUE,
+                             export.name = "F8",
+                             colours=set.colours(6,type = "gradient", gradient.choice = "hot.pink"))
+
+figure_11_data <- fread("C:/Users/alockhart/Desktop/Toronto Survey/Graphs/Figure_11.csv")
+figure_11_data$description <- factor(figure_11_data$description, levels=unique(figure_11_data$description))
+figure.11 <- plot.column.dais(figure_11_data, stat, description, label=TRUE,
+                             plot.fig.num = "Figure 11",
+                             plot.title= graph.data[graph.data$Figure_number=="Figure 11",Figure_title],
+                             y.axis= graph.data[graph.data$Figure_number=="Figure 11",Y_Axis],
+                             caption = graph.data[graph.data$Figure_number=="Figure 11",Caption],
+                             label.unit = graph.data[graph.data$Figure_number=="Figure 11",Y_Axis_Ticks],
+                             label.adjust = 0.025,
+                             export = TRUE,
+                             export.name = "F11")
+
+figure_12_data <- fread("C:/Users/alockhart/Desktop/Toronto Survey/Graphs/Figure_12.csv")
+figure_12_data$description <- factor(figure_12_data$description, levels=unique(figure_12_data$description))
+figure.12 <- plot.column.dais(figure_12_data, stat, year, group.by=description, label=TRUE,
+                              plot.fig.num = "Figure 12",
+                              plot.title= graph.data[graph.data$Figure_number=="Figure 12",Figure_title],
+                              y.axis= graph.data[graph.data$Figure_number=="Figure 12",Y_Axis],
+                              caption = graph.data[graph.data$Figure_number=="Figure 12",Caption],
+                              label.unit = graph.data[graph.data$Figure_number=="Figure 12",Y_Axis_Ticks],
+                              label.adjust = 0.025,
+                              export = TRUE,
+                              export.name = "F12",
+                              colours=set.colours(5,type = "gradient", gradient.choice = "hot.pink"))
+
+figure_14_data <- fread("C:/Users/alockhart/Desktop/Toronto Survey/Graphs/Figure_14.csv")
+figure_14_data$description <- factor(figure_14_data$description, levels=unique(figure_14_data$description))
+figure.14 <- plot.column.dais(figure_14_data, stat, description, label=TRUE,
+                              plot.fig.num = "Figure 14",
+                              plot.title= graph.data[graph.data$Figure_number=="Figure 14",Figure_title],
+                              y.axis= graph.data[graph.data$Figure_number=="Figure 14",Y_Axis],
+                              caption = graph.data[graph.data$Figure_number=="Figure 14",Caption],
+                              label.unit = graph.data[graph.data$Figure_number=="Figure 14",Y_Axis_Ticks],
+                              label.adjust = 0.025,
+                              export = TRUE,
+                              export.name = "F14")
+
+figure_15_data <- fread("C:/Users/alockhart/Desktop/Toronto Survey/Graphs/Figure_15.csv")
+figure_15_data$description <- factor(figure_15_data$description, levels=unique(figure_15_data$description))
+figure.15 <- plot.column.dais(figure_15_data, stat, description, label=TRUE,
+                              plot.fig.num = "Figure 15",
+                              plot.title= graph.data[graph.data$Figure_number=="Figure 15",Figure_title],
+                              y.axis= graph.data[graph.data$Figure_number=="Figure 15",Y_Axis],
+                              caption = graph.data[graph.data$Figure_number=="Figure 15",Caption],
+                              label.unit = graph.data[graph.data$Figure_number=="Figure 15",Y_Axis_Ticks],
+                              label.adjust = 0.025,
+                              export = TRUE,
+                              export.name = "F15")
+
+export.dais.plot("F1", figure.1, p.height = 4.5, p.width = 7.25, type = "pdf")
+export.dais.plot("F3", figure.3, p.height = 4.5, p.width = 7.25, type = "pdf")
+export.dais.plot("F4", figure.4, p.height = 4.5, p.width = 7.25, type = "pdf")
+export.dais.plot("F5", figure.5, p.height = 4.5, p.width = 7.25, type = "pdf")
+export.dais.plot("F6", figure.6, p.height = 4.5, p.width = 7.25, type = "pdf")
+export.dais.plot("F7", figure.7, p.height = 4.5, p.width = 7.25, type = "pdf")
+export.dais.plot("F8", figure.8, p.height = 4.5, p.width = 7.25, type = "pdf")
+export.dais.plot("F11", figure.11, p.height = 4.5, p.width = 7.25, type = "pdf")
+export.dais.plot("F12", figure.12, p.height = 4.5, p.width = 7.25, type = "pdf")
+export.dais.plot("F14", figure.14, p.height = 4.5, p.width = 7.25, type = "pdf")
+export.dais.plot("F15", figure.15, p.height = 4.5, p.width = 7.25, type = "pdf")
+
